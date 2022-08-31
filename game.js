41bc65792af890d224dbf43d5ac6885318c5a435
soukainna <soukaina1lafdili@gmail.com>
2022-08-31 17:12:12 +0200

    add some logic to my js

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("hello")
+
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+}
+
+
