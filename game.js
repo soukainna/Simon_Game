31a295886f9dc99d8ca66ba10a30757b53059645
soukainna <soukaina1lafdili@gmail.com>
2022-08-31 22:44:26 +0200

    add a function to manage sounds

--- game.js
+++ game.js
@@ -3,23 +3,27 @@ var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = []
 var userClickedPattern = []
 
+$(".btn").click(function(e) {
+    console.log(this)
+    var userChosenColour = $(this).attr("id")
+     console.log(userChosenColour)
+     userClickedPattern.push(userChosenColour)
+     console.log(userClickedPattern)
+     playSound(userChosenColour)
+ })
+
+function playSound(name) {
+    var audio = new Audio(`sounds/${name}.mp3`)
+    audio.play()
+}
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
     console.log(randomNumber, randomChosenColour)
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-    $("#" + randomChosenColour).on("click", function() {
-        audio.play();
-    })
-    $("div").click(function(e) {
-        console.log(e)
-        var userChosenColour = e.target.id
-        console.log(userChosenColour)
-        userClickedPattern.push(userChosenColour)
-        console.log(userClickedPattern)
-    })
+    playSound(randomChosenColour)
 }
 
 
